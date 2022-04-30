ab0a1a239fb6ba29e41f25f4dd3ae43b64a19e22
LeenaAlnafea <103067867+LeenaAlnafea@users.noreply.github.com>
2022-05-01 00:20:51 +0300

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,11 +1,13 @@
+
 <?php
 
 $dbhost = "localhost";
 $dbuser = "root";
 $dbpass = "";
 $dbname = "project";
+$dbscore=0;
 
-if(!$con = mysqli_connect($dbhost,$dbuser,$dbpass,$dbname))
+if(!$con = mysqli_connect($dbhost,$dbuser,$dbpass,$dbname,$dbscore))
 {
 
 	die("failed to connect!");
